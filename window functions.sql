efb2bf34a1b4bd49fd36d3bdc36dca7b5cdc8b83
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-06-26 22:55:49 -0700

    final

--- window functions.sql
+++ window functions.sql
@@ -29,3 +29,63 @@ FROM
     DENSE_RANK() OVER (PARTITION BY DepartmentName ORDER BY BaseRate DESC) AS DenseRank
 FROM 
     [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+    -- Example 4 NTILE
+    SELECT 
+    EmployeeKey,
+    FirstName,
+    LastName,
+    DepartmentName,
+    BaseRate,
+    NTILE(4) OVER (PARTITION BY DepartmentName ORDER BY BaseRate DESC) AS Quartile
+FROM 
+    [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+
+    -- Example LAG()
+    SELECT 
+    EmployeeKey,
+    FirstName,
+    LastName,
+    DepartmentName,
+    HireDate,
+    LAG(HireDate, 1) OVER (PARTITION BY DepartmentName ORDER BY HireDate) AS PreviousHireDate
+FROM 
+    [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+    -- Example 6 : LEAD()
+    SELECT 
+    EmployeeKey,
+    FirstName,
+    LastName,
+    DepartmentName,
+    HireDate,
+    LEAD(HireDate, 1) OVER (PARTITION BY DepartmentName ORDER BY HireDate) AS NextHireDate
+FROM 
+    [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+-- Example 7 : SUM() with OVER()
+SELECT 
+    EmployeeKey,
+    FirstName,
+    LastName,
+    DepartmentName,
+    BaseRate,
+    SUM(BaseRate) OVER (PARTITION BY DepartmentName) AS TotalBaseRate
+FROM 
+    [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+
+-- Example 8 AVG with OVER()
+SELECT 
+    EmployeeKey,
+    FirstName,
+    LastName,
+    DepartmentName,
+    BaseRate,
+    AVG(BaseRate) OVER (PARTITION BY DepartmentName) AS AvgBaseRate
+FROM 
+    [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+
+
