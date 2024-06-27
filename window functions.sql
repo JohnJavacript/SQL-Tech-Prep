61871976c915365d3a46ea2b58805783577d618e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-06-26 22:51:07 -0700

    final

--- window functions.sql
+++ window functions.sql
@@ -1,9 +1,20 @@
--- row humber -- 
+-- Example 1  row humber -- 
 SELECT 
     EmployeeKey,
     FirstName,
     LastName,
     DepartmentName,
     ROW_NUMBER() OVER (PARTITION BY DepartmentName ORDER BY HireDate) AS RowNumber
+FROM 
+    [AdventureWorksDW2022].[dbo].[DimEmployee];
+
+-- Example 2 RANK() --
+	SELECT 
+    EmployeeKey,
+    FirstName,
+    LastName,
+    DepartmentName,
+    BaseRate,
+    RANK() OVER (PARTITION BY DepartmentName ORDER BY BaseRate DESC) AS Rank
 FROM 
     [AdventureWorksDW2022].[dbo].[DimEmployee];
